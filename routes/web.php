e3cda7ecd5fd77639a1418feb34f75d2754251a6
Shi-Chin <fred3571588@gmail>
2022-04-25 09:25:29 +0800

    feat:成功版

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
@@ -20,76 +21,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/redirect', function (Request $request) {
     $request->session()->put('state', $state = Str::random(40));
-
-    // $request->session()->put(
-    //     'code_verifier', $code_verifier = Str::random(128)
-    // );
-
-    // $codeChallenge = strtr(rtrim(
-    //     base64_encode(hash('sha256', $code_verifier, true))
-    // , '='), '+/', '-_');
-
     $query = http_build_query([
-        'client_id' => '6',
+        'client_id' => '8',
         'redirect_uri' => 'http://192.168.1.182/project/laravel-client/public/callback',
         'response_type' => 'code',
         'scope' => '',
         'state' => $state,
-        // 'code_challenge' => $codeChallenge,
-        // 'code_challenge_method' => 'S256',
     ]);
     return redirect('http://192.168.1.182/project/laravel-server/public/oauth/authorize?'.$query);
 });
 
 Route::get('/callback', function (Request $request) {
     $state = $request->session()->pull('state');
-    // $codeVerifier = $request->session()->pull('code_verifier');
-
-    // dd($request->session()->all());
-    // throw_unless(
-    //     strlen($state) > 0 && $state === $request->state,
-    //     InvalidArgumentException::class
-    // );
-
     $response = Http::asForm()->post('http://192.168.1.182/project/laravel-server/public/oauth/token', [
         'grant_type' => 'authorization_code',
-        'client_id' => '6',
-        'client_secret' => 'zQfTmUBYMwKBy1tXuASmnaglfUmk7kK6JTWPTFKD',
+        'client_id' => '8',
+        'client_secret' => 'AGKtAormheckwXeagqotOaHATOmgJcXmRMVkUlzw',
         'redirect_uri' => 'http://192.168.1.182/project/laravel-client/public/callback',
-        // 'code_verifier' => $codeVerifier,
         'code' => $request->code,
     ]);
 
     return $response->json();
 });
-
-// Route::get('/redirect', function () {
-//     $query = http_build_query([
-//     'client_id' => '7',
-//     'redirect_uri' => 'http://192.168.1.182/project/laravel-client/public/callback',
-//     'response_type' => 'code',
-//     'scope' => '',
-//     ]);
-
-//     return redirect('http://192.168.1.182/project/laravel-server/public/oauth/authorize?'.$query);
-// });
-
-// Route::get('/callback',function (Request $request) {
-//         // dd($request->all());
-//         $http = new GuzzleHttp\Client;
-//         $response = Http::asForm()->post('http://192.168.1.182/project/laravel-server/public/oauth/token', [
-//             'grant_type' => 'authorization_code',
-//             'client_id' => '7',
-//             'client_secret' => 'zQfTmUBYMwKBy1tXuASmnaglfUmk7kK6JTWPTFKD',      //向server端申請所給的secret
-//             'redirect_uri' => 'http://192.168.1.182/project/laravel-client/public/callback',
-//             'code' => $request->code,
-//     ]);
-//      return $response->json();
-//     }
-// );
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
